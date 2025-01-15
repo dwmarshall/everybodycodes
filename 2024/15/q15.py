631470f7944367f77bf60c8ea2804e85737acd3a
David Marshall <dmarshall@gmail.com>
2025-01-15 14:13:26 -0800

    improve 2024 q15 a bit
    This didn't yield much performance benefit, so maybe it's just
    a really difficult BFS problem. I might try it again as a
    graph problem. ChatGPT suggested "Ant Colony Optimization,"
    which looks interesting.

--- 2024/15/q15.py
+++ 2024/15/q15.py
@@ -1,7 +1,3 @@
-from collections import defaultdict
-from functools import cache
-from itertools import pairwise
-import heapq
 import string
 
 DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
@@ -29,78 +25,36 @@ def input(
     return tuple(grid), starting_position, herbs
 
 
-def path_length(g: tuple[tuple[str]], path: tuple[tuple[int, int]]) -> int:
-    total_length = 0
-    for a, b in pairwise(path):
-        total_length += minimum_steps(g, a, b)
-    return total_length
-
-
-def remaining_herbs(
-    g: tuple[tuple[str]], h: dict[tuple[int, int], str], path: tuple[tuple[int, int]]
-) -> set[str]:
-    herbs = set(h.values())
-    for p in path:
-        x, y = p
-        if g[y][x] in herbs:
-            herbs.remove(g[y][x])
-    return herbs
-
-
 def solve(
     g: tuple[tuple[str]], h: dict[tuple[int, int], str], origin: tuple[int, int]
 ) -> int:
-    q = []
     initial_state = (
         origin,  # position
         frozenset(),  # herbs acquired
     )
-    visited = defaultdict(lambda: float("inf"))
-    least_steps = float("inf")
-    heapq.heappush(q, (0, 0, initial_state))
-    while q:
-        # print(len(q), least_steps, q[0])
-        _, steps, state = heapq.heappop(q)
-        if steps > visited[state]:
-            continue
-        (x, y), herbs_acquired = state
-        if (x, y) == origin and herbs_acquired == frozenset(h.values()):
-            least_steps = min(least_steps, steps)
-        for dx, dy in DIRECTIONS:
-            nx, ny = x + dx, y + dy
-            if 0 <= ny < len(g) and 0 <= nx < len(g[0]) and g[ny][nx] not in "#~":
-                new_herbs_acquired = herbs_acquired
-                if (nx, ny) in h:
-                    new_herbs_acquired = herbs_acquired | {h[(nx, ny)]}
-                state = ((nx, ny), frozenset(new_herbs_acquired))
-                if steps + 1 < visited[state]:
-                    visited[state] = steps + 1
-                    heapq.heappush(
-                        q,
-                        (-len(new_herbs_acquired), steps + 1, state),
-                    )
-    return least_steps
-
-
-@cache
-def minimum_steps(
-    g: tuple[tuple[str]], start_at: tuple[int, int], end_at: tuple[int, int]
-) -> int:
+    states = {initial_state}
     visited = set()
-    q = []
-    heapq.heappush(q, (0, start_at))
-    while q:
-        steps, current_position = heapq.heappop(q)
-        if current_position == end_at:
-            return steps
-        if current_position in visited:
-            continue
-        visited.add(current_position)
-        x, y = current_position
-        for dx, dy in DIRECTIONS:
-            nx, ny = x + dx, y + dy
-            if 0 <= ny < len(g) and 0 <= nx < len(g[0]) and g[ny][nx] not in "#~":
-                heapq.heappush(q, (steps + 1, (nx, ny)))
+    steps = 0
+    while True:
+        print(f"At step {steps}, there are {len(states)} states.")
+        new_states = set()
+        for current_state in states:
+            if current_state in visited:
+                continue
+            visited.add(current_state)
+            (x, y), herbs_acquired = current_state
+            if (x, y) == origin and herbs_acquired == frozenset(h.values()):
+                return steps
+            for dx, dy in DIRECTIONS:
+                nx, ny = x + dx, y + dy
+                if 0 <= ny < len(g) and 0 <= nx < len(g[0]) and g[ny][nx] not in "#~":
+                    new_herbs_acquired = herbs_acquired
+                    if (nx, ny) in h:
+                        new_herbs_acquired = herbs_acquired | {h[(nx, ny)]}
+                    new_state = ((nx, ny), frozenset(new_herbs_acquired))
+                    new_states.add(new_state)
+        states = new_states
+        steps += 1
 
 
 for p in range(1, 4):
